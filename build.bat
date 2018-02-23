7b6739d876da6fc38bd3e47e2519741ca11021a3
Arthur Landim <landim.arthur@gmail.com>
2018-02-23 09:08:38 -0300

    Use nuget packages

--- build.bat
+++ build.bat
@@ -5,22 +5,6 @@ set branch=%2
 set framework=%3
 echo.Driver: %driver%
 echo.Branch/Tag: %branch%
-if not exist csharp-dse-driver git clone https://github.com/riptano/csharp-dse-driver.git
-if not exist csharp-driver git clone https://github.com/datastax/csharp-driver.git
-if "%driver%"=="dse" (
-	cd csharp-dse-driver
-	git fetch origin %branch%
-	git reset --hard %branch%
-	msbuild /v:q /nologo /property:Configuration=Release src\Dse.sln
-)
-git fetch origin
-if "%driver%"=="cassandra" (
-	cd csharp-driver
-	git fetch origin %branch%
-	git reset --hard %branch%
-	msbuild /v:q /nologo /property:Configuration=Release src\Cassandra.sln
-)
-cd ..
 rem Required for package restore
 dotnet restore "src\DataStax.Driver.Benchmarks\DataStax.Driver.Benchmarks.sln" -v minimal
 dotnet clean "src\DataStax.Driver.Benchmarks\DataStax.Driver.Benchmarks.sln" -v minimal
